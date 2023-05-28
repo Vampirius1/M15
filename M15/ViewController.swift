dbbeaaecffa12bd8f0c3a0f77511044c8a2ec9ff
Vampirius1 <111462054+Vampirius1@users.noreply.github.com>
2023-05-28 11:53:27 +0300

    Initial Commit

---
+++ M15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M15
+//
+//  Created by Владислав on 28.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
